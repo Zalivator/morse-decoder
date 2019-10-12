5b187910036a3d12f21d486963267e66fc842252
Zalya <pzalivako@mail.ru>
2019-10-12 09:05:38 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let length = expr.length;
+    let str = '';
+    let sign = '';
+    let space = '**********';
+    let tenOrEleven = '';
+    for (let i = 0; i < length; i += 10) {
+        sign = expr.slice(i, i + 10);
+        if (sign == space) { 
+            str += ' ';
+        }
+        else {
+            let signStr = '';
+            for (let j = 0; j < sign.length; j += 2) {
+                tenOrEleven = sign.slice(j, j + 2);
+                if (tenOrEleven == 10) {
+                    signStr += '.';
+                }
+                else if (tenOrEleven == 11) {
+                    signStr += '-';
+                }
+            }
+            str += MORSE_TABLE[signStr];
+        }
+    }
+
+    return str;
 }
 
 module.exports = {
